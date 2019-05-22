5c565e4577df877f283005daee90b4b3c3c44cf6
thearifismail <arifismail@yahoo.com>
2019-05-22 14:35:31 -0400

    Added cluster validation

--- spec/models/transformation_mapping_item_spec.rb
+++ spec/models/transformation_mapping_item_spec.rb
@@ -1,40 +1,35 @@
 RSpec.describe TransformationMappingItem, :v2v do
-  let(:ems_vmware) { FactoryBot.create(:ems_vmware) }
-  let(:vmware_cluster) { FactoryBot.create(:ems_cluster, :ext_management_system => ems_vmware) }
+  let(:vmware_cluster) { FactoryBot.create(:ems_cluster, :vmware_ems) }
+  let(:redhat_cluster) { FactoryBot.create(:ems_cluster, :redhat_ems) }
+  let(:openstack_cluster) { FactoryBot.create(:ems_cluster_openstack, :openstack_ems) }
 
-  let(:ems_redhat) { FactoryBot.create(:ems_redhat) }
-  let(:redhat_cluster) { FactoryBot.create(:ems_cluster, :ext_management_system => ems_redhat) }
-
-  let(:ems_openstack) { FactoryBot.create(:ems_openstack) }
-  let(:openstack_cluster) { FactoryBot.create(:ems_cluster_openstack, :ext_management_system => ems_openstack) }
-
-  context "source cluster validation" do
-    let(:valid_mapping_item) do
+   context "source cluster validation" do
+    let(:valid_mapping_item) {
       FactoryBot.build(:transformation_mapping_item, :source => vmware_cluster, :destination => openstack_cluster)
-    end
+    }
 
-    let(:invalid_mapping_item) do
+     let(:invalid_mapping_item) {
       FactoryBot.build(:transformation_mapping_item, :source => openstack_cluster, :destination => openstack_cluster)
-    end
+    }
 
-    it "passes validation if the source cluster is not a supported type" do
+     it "passes validation if the source cluster is not a supported type" do
       expect(valid_mapping_item.valid?).to be true
     end
 
-    it "fails validation if the source cluster is not a supported type" do
+     it "fails validation if the source cluster is not a supported type" do
       expect(invalid_mapping_item.valid?).to be false
       expect(invalid_mapping_item.errors[:source].first).to match("EMS type of source cluster must be in")
     end
   end
 
   context "destination cluster validation" do
-    let(:valid_mapping_item) do
+    let(:valid_mapping_item) {
       FactoryBot.build(:transformation_mapping_item, :source => vmware_cluster, :destination => redhat_cluster)
-    end
+    }
 
-    let(:invalid_mapping_item) do
+    let(:invalid_mapping_item) {
       FactoryBot.build(:transformation_mapping_item, :source => vmware_cluster, :destination => vmware_cluster)
-    end
+    }
 
     it "passes validation if the source cluster is not a supported type" do
       expect(valid_mapping_item.valid?).to be true
